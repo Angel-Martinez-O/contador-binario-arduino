2bfd58b93d672cc98f9b8f22e73a82c5377f527c
Angel-Martinez-O <agm3709@gmail.com>
2026-09-12 12:43:19 -0500

    Add files via upload

---
+++ contador_velocidad.ino
@@ -0,0 +1,23 @@
+const int leds[] = {2, 4, 7, 8}; 
+const int numLeds = 4;
+int waitDelay = 500;   // tiempo entre cada número
+
+void setup() {
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int contador = 0; contador < 16; contador++) {
+    mostrarBinario(contador);
+    delay(waitDelay);
+  }
+}
+
+void mostrarBinario(int numero) {
+  for (int i = 0; i < numLeds; i++) {
+    int estadoBit = bitRead(numero, i);
+    digitalWrite(leds[i], estadoBit);
+  }
+}
